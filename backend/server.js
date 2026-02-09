6a3e2b62898db19bbcec19779324be242ec6162f
Dhanush D <kamatchidharanikumar@gmail.com>
2026-02-09 14:34:10 +0530

    Fix backend server and RBAC logic

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./db");
+const path = require("path");
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
